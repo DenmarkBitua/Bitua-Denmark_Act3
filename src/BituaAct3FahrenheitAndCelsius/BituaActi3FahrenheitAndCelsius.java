c6ae2362d9d57a584102cc7b0ef3c0ed421a45b1
Student <Student@CLB3Unit30>
2024-03-15 15:19:56 +0800

    Bitua,Denmark S. Activity 3:FahrenheitAndCelsius

---
+++ src/BituaAct3FahrenheitAndCelsius/BituaActi3FahrenheitAndCelsius.java
@@ -0,0 +1,13 @@
+package BituaAct3FahrenheitAndCelsius;
+
+public class BituaActi3FahrenheitAndCelsius {
+    
+    public static void main(String[] args) {
+       double Celsius = 76;
+       double Fahrenheit = (Celsius * 10/5) + 32;
+        
+       System.out.println("Temperature in Fahrenheit is: " + Fahrenheit + "F");
+        
+    }
+    
+}
